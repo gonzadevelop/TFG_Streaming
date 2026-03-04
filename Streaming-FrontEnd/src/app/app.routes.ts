a61adbd737accd59a951fb9fe0121edcb4669cab
Kike Ruiz <kikeruuuiz@gmail.com>
2026-03-04 19:11:27 +0100

    Interfaz login v1.

--- Streaming-FrontEnd/src/app/app.routes.ts
+++ Streaming-FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./componentes/zonaCliente/Auth/login/login').then(m => m.Login),
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+];
