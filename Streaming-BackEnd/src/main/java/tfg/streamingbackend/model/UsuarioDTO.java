59659fdf418e189e85dc1b8a2cf1c3aab061a9e4
gonzadevelop <gonzalovicente@proton.me>
2026-03-08 19:24:04 +0100

    endpoint de info-usuario creado

---
+++ Streaming-BackEnd/src/main/java/tfg/streamingbackend/model/UsuarioDTO.java
@@ -0,0 +1,10 @@
+package tfg.streamingbackend.model;
+
+import lombok.Data;
+
+@Data
+public class UsuarioDTO {
+    private String username;
+    private String email;
+    private String urlAvatar;
+}
