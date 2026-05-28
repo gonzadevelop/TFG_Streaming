6a558be9465d07d9037eea92b5e71d6cb8fed9b9
gonzadevelop <gonzalovicente@proton.me>
2026-05-28 22:23:11 +0200

    envoirment file cambiado

--- Streaming-FrontEnd/src/app/environments/environment.ts
+++ Streaming-FrontEnd/src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiURL: 'https://spring-backend-603923626139.europe-west4.run.app'
+  apiURL: 'https://spring-backend-603923626139.europe-west4.run.app/api'
 };
 
 
